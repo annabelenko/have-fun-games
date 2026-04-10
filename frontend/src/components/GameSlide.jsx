25fc7641d156ffa1d720eee34830286ddec9da55
CaptCaptain <55301501+CaptCaptain@users.noreply.github.com>
2026-04-10 16:01:35 +0000

    Added base layout of game description

--- frontend/src/components/GameSlide.jsx
+++ frontend/src/components/GameSlide.jsx
@@ -2,13 +2,22 @@ import "keen-slider/keen-slider.min.css";
 import "./GameSlide.css";
 
 function GameSlide() {
+	// TODO: Implement thumbnail and description sections.
 	return (
 		<>
-			// TODO: Implement thumbnail and description sections.
 			<div className="game-slide">
 				<div className="keen-slider__slide number-slide1">
 					<div className="slide-element slide-thumbnail">Thumbnail</div>
-					<div className="slide-element slide-description">Game Title</div>
+					<div
+						className="slide-element number-slide2 slide-description"
+						style={{ display: "flex", flexDirection: "column" }}
+					>
+						<div>Game Title</div>
+						<div style={{ display: "flex", gap: "20px" }}>
+							<div>$0.99</div>
+							<div>Windows</div>
+						</div>
+					</div>
 				</div>
 			</div>
 		</>
