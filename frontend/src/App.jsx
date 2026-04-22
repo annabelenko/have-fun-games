108e5338b337b9683d17ec7713e61e2f968e02eb
CaptCaptain <55301501+CaptCaptain@users.noreply.github.com>
2026-04-22 12:09:17 +0000

    Revert "Fixed missing React import"
    This reverts commit 9e715f24a45c045f1e8e7dd9dcdc1bc4aefad50f.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,17 @@
-import React from "react";
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
-import Login from "./pages/Login";
-import Signup from "./pages/Signup";
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
+import Login from './pages/Login'
+import Signup from './pages/Signup'
 
 export default function App() {
-	return (
-		<BrowserRouter>
-			<nav>
-				<Link to="/login">Login</Link> | <Link to="/signup">Signup</Link>
-			</nav>
-			<Routes>
-				<Route path="/login" element={<Login />} />
-				<Route path="/signup" element={<Signup />} />
-			</Routes>
-		</BrowserRouter>
-	);
+  return (
+    <BrowserRouter>
+      <nav>
+        <Link to="/login">Login</Link> | <Link to="/signup">Signup</Link>
+      </nav>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
