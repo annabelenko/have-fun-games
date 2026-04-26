119c14fb33860ded483b64599671b2f7b933b0f6
CaptCaptain <55301501+CaptCaptain@users.noreply.github.com>
2026-04-26 16:32:25 +0000

    Added fetching of additional game info

--- frontend/src/pages/GamePage.jsx
+++ frontend/src/pages/GamePage.jsx
@@ -6,7 +6,6 @@ import ErrorPage from "./ErrorPage";
 
 import HeroCanvas from "../components/HeroCanvas";
 import ItemPrice from "../components/ItemPrice";
-import OSPlatform from "../components/OSPlatform";
 
 import PurchaseButton from "../components/PurchaseButton";
 import Thumbnail from "../components/Thumbnail";
@@ -15,11 +14,14 @@ import styles from "./GamePage.module.css";
 
 export default function GamePage() {
 	const params = useParams();
-	const [gameTitle, setGameTitle] = useState(`[ Item ${params.id} ]`);
-	const [gameDescription, setGameDescription] = useState(
-		`[ Item ${params.id} ]`,
-	);
+	const [gameTitle, setGameTitle] = useState(``);
+	const [gameDescription, setGameDescription] = useState(``);
 	const [gamePrice, setGamePrice] = useState(0);
+	const [gameCover, setGameCover] = useState(``);
+	const [gamePreview, setGamePreview] = useState(``);
+	const [gameReleaseDate, setGameReleaseDate] = useState(``);
+	const [gameDeveloper, setGameDeveloper] = useState(``);
+	const [dataReady, setDataReady] = useState(false);
 
 	if (!params.id)
 		return (
@@ -33,16 +35,30 @@ export default function GamePage() {
 		setGameTitle(data.title);
 		setGameDescription(data.description);
 		setGamePrice(data.price);
+		setGameCover(data.cover_image_url);
+		setGamePreview(data.micro_trailer_url);
+		setGameReleaseDate(data.release_date);
+		setGameDeveloper(data.developer);
+		setDataReady(true);
 	});
 
 	return (
 		<>
 			<div className={styles.center}>
-				<div className={styles.gamePage}>
+				<div className={`${styles.gamePage} ${dataReady ? "" : styles.hidden}`}>
 					<div className={styles.gamePageContent}>
 						<div
 							className={`${styles.gamePageSection} ${styles.gamePageSectionLeft}`}
 						>
+							{/* Game Cover */}
+							<img
+								src={gameCover}
+								style={{
+									aspectRatio: 16 / 9,
+									width: "100%",
+									objectFit: "contain",
+								}}
+							/>
 							{/* Purchase Modal */}
 							<div className={styles.purchaseModal}>
 								<div className={styles.purchaseModalTitle}>Buy {gameTitle}</div>
@@ -53,6 +69,7 @@ export default function GamePage() {
 											display: "flex",
 											justifyContent: "left",
 											width: "50%",
+											marginTop: "50px",
 										}}
 									>
 										<PurchaseButton />
@@ -63,25 +80,36 @@ export default function GamePage() {
 											justifyContent: "right",
 											width: "50%",
 										}}
-									>
-										<OSPlatform Windows Mac Linux />
-									</div>
+									></div>
 								</div>
 							</div>
-							{/* Game Description */}
-							<div>{gameDescription}</div>
 						</div>
 						<div
 							className={`${styles.gamePageSection} ${styles.gamePageSectionRight}`}
 						>
-							<Thumbnail />
-							<Thumbnail />
-							<Thumbnail />
+							{/* Game Trailer */}
+							<video
+								style={{ width: "100%" }}
+								autoplay
+								playsinline
+								controls
+								muted
+								loop
+								src={gamePreview}
+							/>
+							<div style={{ display: "flex", flexDirection: "column" }}>
+								<p>Release Date: {gameReleaseDate}</p>
+								<p>Developer: {gameDeveloper}</p>
+							</div>
 						</div>
+					</div>
+					{/* Game Description */}
+					<div style={{ padding: "10px 60px 60px 60px" }}>
+						{gameDescription}
 					</div>
 				</div>
 			</div>
-			<div className={styles.background}>
+			<div className={`${styles.background} ${dataReady ? "" : styles.hidden}`}>
 				<HeroCanvas />
 			</div>
 		</>
