139d9944c469d0fd5e116697e136c3fb26900a6a
anna belenko <66851299+annabelenko@users.noreply.github.com>
2026-04-25 23:10:20 -0400

    fixed ui, maroon + yellow for login page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,11 @@
 import React from "react";
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 
 export default function App() {
 	return (
 		<BrowserRouter>
-			<nav>
-				<Link to="/login">Login</Link> | <Link to="/signup">Signup</Link>
-			</nav>
 			<Routes>
 				<Route path="/login" element={<Login />} />
 				<Route path="/signup" element={<Signup />} />
