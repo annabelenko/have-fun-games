08509da832e6d160ab42192252ea1940656d0d16
CaptCaptain <55301501+CaptCaptain@users.noreply.github.com>
2026-04-26 13:34:24 +0000

    Added GamePage and ErrorPage to routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,8 @@ import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
+import GamePage from "./pages/GamePage";
+import ErrorPage from "./pages/ErrorPage";
 import "./App.css";
 
 export default function App() {
@@ -14,8 +16,12 @@ export default function App() {
 					<span className="header-title">Have Fun Games</span>
 				</Link>
 				<nav className="header-nav">
-					<Link to="/login" className="header-nav-link">Login</Link>
-					<Link to="/signup" className="header-nav-cta">Sign Up</Link>
+					<Link to="/login" className="header-nav-link">
+						Login
+					</Link>
+					<Link to="/signup" className="header-nav-cta">
+						Sign Up
+					</Link>
 				</nav>
 			</header>
 
@@ -23,6 +29,12 @@ export default function App() {
 				<Route path="/" element={<HomePage />} />
 				<Route path="/login" element={<Login />} />
 				<Route path="/signup" element={<Signup />} />
+				<Route path="/game" element={<GamePage />} />
+				<Route path="/game/:id" element={<GamePage />} />
+				<Route
+					path="/*"
+					element={<ErrorPage message="Page does not exist" />}
+				/>
 			</Routes>
 		</BrowserRouter>
 	);
